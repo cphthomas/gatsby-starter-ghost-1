705a756cb9b7f58f2c083bf398fc562f93e32261
007AMAN007 <as47986@gmail.com>
2021-03-09 22:47:29 +0530

    stripe

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,4 +1,12 @@
+const stripe = require("stripe")("sk_test_6uOkcnnJw0VAoDZmIaKWEqzu");
+
 exports.handler = async function () {
+    const customer = await stripe.customers.create({
+        email: "customer@example.com",
+    });
+
+    console.log(customer.id);
+
     return {
         statusCode: 200,
         body: "Hello world!",
