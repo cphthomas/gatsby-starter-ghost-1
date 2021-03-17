a237af29c0a246755772c6265146b1b1bec39ca9
007AMAN007 <as47986@gmail.com>
2021-03-17 22:38:03 +0530

    part-2

--- functions/sign-up.js
+++ functions/sign-up.js
@@ -7,7 +7,11 @@ exports.handler = async function (event) {
         email: email,
     });
 
-    //console.log(customer.id);
+    // subscribe the new customer to the free plan
+    await stripe.subscriptions.create({
+        customer: customer.id,
+        items: [{ price: "price_1IKAnIIP8uHvYRBy68pRrArU" }],
+    });
 
     return {
         statusCode: 200,
