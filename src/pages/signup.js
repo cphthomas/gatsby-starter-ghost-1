8691b2f4d200a64747ce6a7b5a42bc7fe6d198e5
007AMAN007 <as47986@gmail.com>
2021-03-30 13:57:19 +0530

    ccl

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-// import "./Login.css";
 import { Layout } from "../components/common";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/login.css";
