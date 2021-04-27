c1ad997a3247a9cd03abd0d533c614f4107fcf15
cphthomas <cphthomas@gmail.com>
2021-04-27 09:58:41 +0200

    thomas1

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,11 +7,4 @@ $(function () {
     console.log("in function ready!");
 });
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+
