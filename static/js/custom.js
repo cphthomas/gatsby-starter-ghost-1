d3bd286b95bc29a94a111eb662b89ad7f1530b52
007AMAN007 <as47986@gmail.com>
2021-05-08 21:51:24 +0530

    search resting

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,13 +9,3 @@ $(function () {
 });
 
 $(".toc").load(function () {});
-
-(function () {
-    var id = "f3658966-ae48-11eb-a9b5-0242ac130002";
-    var ci_search = document.createElement("script");
-    ci_search.type = "text/javascript";
-    ci_search.async = true;
-    ci_search.src = "https://cse.expertrec.com/api/js/ci_common.js?id=" + id;
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(ci_search, s);
-})();
