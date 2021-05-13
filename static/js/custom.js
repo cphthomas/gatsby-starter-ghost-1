b67302ccb6a8b4b9c5128aa49385464c5f302961
cphthomas <cphthomas@gmail.com>
2021-05-13 11:16:32 +0200

    vis.js aman help

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,6 +2,25 @@ $(document).ready(function () {
     console.log("in document ready!");
     $("body").tooltip({ selector: "[data-toggle=tooltip]" });
     console.log("in ready");
+    var nodes = [
+        {
+          id:"a",
+          label:"A"
+        },
+        {
+          id:"b",
+          label:"B"
+        }
+ ];
+ var edges = [
+        {
+          from:"a",
+          to:"b"
+        }
+ ];
+     var options = {};
+     var container = document.querySelector('.network');
+     network = new vis.Network(container, data, options);
 });
 
 $(function () {
