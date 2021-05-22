319f32b7c4353920b8fe70f697dccf9ceeb4c726
cphthomas <cphthomas@gmail.com>
2021-05-22 21:10:33 +0200

    mm

--- src/custom.js
+++ src/custom.js
@@ -59,20 +59,10 @@ function waitForElement(querySelector, timeout = 0) {
 
 function visScript() {
     var nodes = new vis.DataSet([
-        { id: 1, value: 20, label: "1" },
-        { id: 2, value: 5, label: "2" },
-        { id: 3, value: 10, label: "3" },
-        { id: 4, value: 15, label: "4" },
-        { id: 5, value: 8, label: "5" },
-        { id: 6, value: 10, label: "6" },
-        { id: 7, value: 50, label: "7" },
-        { id: 8, value: 10, label: "8" },
-        { id: 9, value: 10, label: "9" },
-        { id: 10, value: 10, label: "10" },
-        { id: 11, value: 10, label: "11" },
-        { id: 12, value: 4, label: "12" },
-        { id: 13, value: 2, label: "13" },
-        { id: 14, value: 1, label: "14" },
+        { id: 1, value: 10,level: "1", label: "Lovgivende magt \n Folketinget" },
+        { id: 2, value: 10,level: "2", label: "Dømmende magt \n Domstolene" },
+        { id: 3, value: 10,level: "2", label: "Udøvende magt \n Regeringen" },
+        
     ]);
     var nodes2 = new vis.DataSet([
         { id: 1, value: 1, label: "23" },
@@ -94,21 +84,9 @@ function visScript() {
     // create an array with edges
     var edges = new vis.DataSet([
         { from: 1, to: 2 },
-        { from: 1, to: 4 },
-        { from: 1, to: 5 },
-        { from: 2, to: 6 },
-        { from: 2, to: 7 },
-        { from: 2, to: 8 },
-        { from: 3, to: 9 },
-        { from: 3, to: 10 },
-        { from: 3, to: 11 },
-        { from: 10, to: 12 },
-        { from: 10, to: 13 },
-        { from: 10, to: 14 },
-        { from: 7, to: 11 },
-        { from: 8, to: 14 },
-        { from: 11, to: 12 },
-        { from: 5, to: 9 },
+        { from: 2, to: 3 },
+        { from: 3, to: 1 },
+        
     ]);
     var edges2 = new vis.DataSet([
         { from: 1, to: 2 },
@@ -147,9 +125,10 @@ function visScript() {
             autoResize: true,
             height: "100%",
             width: "100%",
-            shape: "circle",
+            shape: "box",
             font: {
-                size: 30,
+                color: "white",
+                size: 10,
             },
             scaling: {
                 label: {
@@ -158,27 +137,34 @@ function visScript() {
                 },
             },
             borderWidth: 1,
-            shadow: true,
+            shadow: false,
             margin: {
                 top: 10,
-                left: 20,
-                right: 20,
+                left: 5,
+                right: 5,
                 bottom: 10,
             },
             color: {
-                border: "",
-                background: "#b2dfdb",
+                
+                border: "black",
+                background: "black",
                 highlight: {
-                    border: "#e57373",
-                    background: "#ffcdd2",
+                    fontColor:"black",
+                    
+                    border: "orange",
+                    background: "orange",
                 },
             },
         },
         layout: {
             improvedLayout: true,
             hierarchical: {
-                enabled: false,
+                enabled: true,
                 direction: "UD",
+                levelSeparation: 200,
+      nodeSpacing: 10,
+      treeSpacing: 10,
+      blockShifting: true,
                 sortMethod: "hubsize",
                 parentCentralization: true,
                 blockShifting: true,
@@ -186,17 +172,17 @@ function visScript() {
             },
         },
         edges: {
-            smooth: true,
+            smooth: false,
             chosen: true,
             arrows: {
                 to: {
-                    enabled: true,
+                    enabled: false,
                     type: "arrow",
                 },
             },
             color: {
-                color: "#b2dfdb",
-                highlight: "#ffcdd2",
+                color: "black",
+                highlight: "orange",
                 hover: "#848484",
                 inherit: "from",
                 opacity: 1.0,
--- src/html.js
+++ src/html.js
@@ -42,6 +42,7 @@ export default function HTML(props) {
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.10.0/tocbot.min.js"></script>
+                <script src="https://widget.spreaker.com/widgets.js" async></script>  
                 <script
                     type="text/javascript"
                     src={withPrefix("js/custom.js")}
