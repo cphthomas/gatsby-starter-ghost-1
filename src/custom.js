a8968c2af9cc02041930797c1074d29440af09f1
cphthomas <cphthomas@gmail.com>
2021-05-25 08:39:13 +0200

    vis

--- src/custom.js
+++ src/custom.js
@@ -1,7 +1,7 @@
 const customJS = () => {
     $("body").tooltip({ selector: "[data-toggle=tooltip]" });
 
-    waitForElement("#mynetwork1, #mynetwork2", 8000)
+    waitForElement("#vis11, #vis12","#vis13","#vis31", 8000)
         .then(function () {
             console.log("element is loaded.. do stuff");
             visScript();
@@ -38,53 +38,105 @@ function waitForElement(querySelector, timeout = 0) {
 }
 
 function visScript() {
-    var nodes = new vis.DataSet([
+    var nodes11 = new vis.DataSet([
         {
             id: 1,
             value: 10,
             level: "1",
-            label: "Lovgivende magt \n Folketinget",
+            label: "Lovgivende\nmagt\nFolketinget",
         },
-        { id: 2, value: 10, level: "2", label: "Dømmende magt \n Domstolene" },
-        { id: 3, value: 10, level: "2", label: "Udøvende magt \n Regeringen" },
+        { id: 2, value: 10, level: "2", label: "Dømmende\nmagt\nDomstolene" },
+        { id: 3, value: 10, level: "2", label: "Udøvende\nmagt\nRegeringen" },
     ]);
-    var nodes2 = new vis.DataSet([
-        {
-            id: 1,
-            value: 10,
-            level: "1",
-            label: "node2 label1",
-        },
-        { id: 2, value: 10, level: "1", label: "node2 label2" },
-        { id: 3, value: 10, level: "2", label: "node2 label3" },
-        { id: 4, value: 10, level: "3", label: "node2 label4" },
+    var nodes12 = new vis.DataSet([
+        { id: 1, font: { size: 20 },level: "1", label: "Højesteret" },
+        { id: 2,  font: { size: 18 },level: "2", label: "Vestre\nLandsret" },
+        { id: 3,  font: { size: 18 },level: "2", label: "Østre\nLandsret" },
+        { id: 4,  font: { size: 18 },level: "3", label: "Byretterne" },
+        { id: 5,  font: { size: 12 },level: "4", label: "Sø og\nhandelsretten" },
+        { id: 6,  font: { size: 12 },level: "4", label: "Grønlands-\ndomstolen" },
+        { id: 7,  font: { size: 12 },level: "4", label: "Retten på\nFærøerne" },
+        { id: 8,  font: { size: 12 },level: "1", label: " Procesbevillingsnævnet \n Den særlige klageret \n Dommerudnævnelsesrådet \n Bibeskæftigelsesnævnet" },
+    ]);
+    var nodes13 = new vis.DataSet([
+        { id: 1, font: { size: 25 }, level: "1", label: "Byrettens afdelinger" },
+        { id: 2, font: { size: 14 }, level: "2", label: "Civilretten" },
+        { id: 3, font: { size: 14 }, level: "2", label: "Fogedretten" },
+        { id: 4, font: { size: 14 }, level: "2", label: "Skifteretten" },
+        { id: 5, font: { size: 14 }, level: "2", label: "Boligretten" },
+        { id: 6, font: { size: 14 }, level: "2", label: "Kriminalretten" },
+    ]);
+    var nodes31 = new vis.DataSet(
+        options = 
+        [
+        { id: 1, value: 10, level: "1", label: "Aftaleloven" },
+        { id: 2, value: 10, level: "2", label: "Indgåelse af forsikringsaftaler" },
+        { id: 3, value: 10, level: "2", label: "Indgåelse af kreditaftaler" },
+        { id: 4, value: 10, level: "2", label: "Forbrugeraftaler" },
+        { id: 5, value: 10, level: "2", label: "Aftaler om pant" },
+        { id: 6, value: 10, level: "2", label: "Aftaler om køb" },
+        { id: 7, value: 10, level: "2", label: "Aftaler om kautionsformer" },
     ]);
 
     // create an array with edges
-    var edges = new vis.DataSet([
+    var edges11 = new vis.DataSet([
         { from: 1, to: 2 },
         { from: 2, to: 3 },
         { from: 3, to: 1 },
+        
+       
     ]);
-    var edges2 = new vis.DataSet([
+    var edges12 = new vis.DataSet([
         { from: 1, to: 2 },
-        { from: 2, to: 3 },
-        { from: 3, to: 1 },
+        { from: 1, to: 3 },
+        { from: 1, to: 4 },
+        { from: 2, to: 4 },
         { from: 3, to: 4 },
+        { from: 2, to: 5 },
+        { from: 3, to: 5 },
+        { from: 2, to: 6 },
+        { from: 3, to: 7 },
+    ]);
+    var edges13 = new vis.DataSet([
+        { from: 1, to: 2 },
+        { from: 1, to: 6 },
+        { from: 1, to: 3 },
+        { from: 1, to: 4 },
+        { from: 1, to: 5 },
+    ]);
+    var edges31 = new vis.DataSet([
+        { from: 1, to: 2 },
+        { from: 1, to: 3 },
+        { from: 1, to: 4 },
+        { from: 1, to: 5 },
+        { from: 1, to: 6 },
     ]);
 
     // create a network
-    var container = document.getElementById("mynetwork");
-    var container2 = document.getElementById("mynetwork2");
+    var container11 = document.getElementById("vis11");
+    var container12 = document.getElementById("vis12");
+    var container13 = document.getElementById("vis13");
+    var container31 = document.getElementById("vis31");
+    
 
     // provide the data in the vis format
-    var data = {
-        nodes: nodes,
-        edges: edges,
+    var data11 = {
+        nodes: nodes11,
+        edges: edges11,
     };
-    var data2 = {
-        nodes: nodes2,
-        edges: edges2,
+    var data12 = {
+        nodes: nodes12,
+        edges: edges12,
+    };
+    var data13 = {
+        nodes: nodes13,
+        edges: edges13,
+    };
+    var data31 = {
+        
+        
+        nodes: nodes31,
+        edges: edges31,
     };
     var options = {
         nodes: {
@@ -129,8 +181,8 @@ function visScript() {
                 springLength: 230,
                 springConstant: 0.18,
                 avoidOverlap: -10,
-                treeSpacing: 1000,
-                nodeDistance: 200,
+                treeSpacing: 100,
+                nodeDistance: 100,
             },
             maxVelocity: 146,
             solver: "hierarchicalRepulsion",
@@ -145,7 +197,7 @@ function visScript() {
             hierarchical: {
                 enabled: true,
                 direction: "UD",
-                levelSeparation: 200,
+                levelSeparation: 120,
                 nodeSpacing: 10,
                 treeSpacing: 10,
                 blockShifting: true,
@@ -175,8 +227,11 @@ function visScript() {
     };
 
     // initialize your network!
-    var network = new vis.Network(container, data, options);
-    var network2 = new vis.Network(container2, data2, options);
+    var vis11 = new vis.Network(container11, data11, options);
+    var vis12 = new vis.Network(container12, data12, options);
+    var vis13 = new vis.Network(container13, data13, options);
+    var vis31 = new vis.Network(container31, data31, options);
+    
 }
 
 export default customJS;
