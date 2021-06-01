12d747c872fda8ff9a8474d946a0672c5c5ba9ef
007AMAN007 <as47986@gmail.com>
2021-06-01 16:53:13 +0530

    new site branch

--- src/custom.js
+++ src/custom.js
@@ -1,7 +1,7 @@
 const customJS = () => {
     $("body").tooltip({ selector: "[data-toggle=tooltip]" });
 
-    waitForElement("#vis11, #vis12","#vis13","#vis31", 8000)
+    waitForElement("#vis11, #vis12", "#vis13", "#vis31", 8000)
         .then(function () {
             console.log("element is loaded.. do stuff");
             visScript();
@@ -39,17 +39,38 @@ function visScript() {
         { id: 3, value: 10, level: "2", label: "Udøvende\nmagt\nRegeringen" },
     ]);
     var nodes12 = new vis.DataSet([
-        { id: 1, font: { size: 20 },level: "1", label: "Højesteret" },
-        { id: 2,  font: { size: 18 },level: "2", label: "Vestre\nLandsret" },
-        { id: 3,  font: { size: 18 },level: "2", label: "Østre\nLandsret" },
-        { id: 4,  font: { size: 18 },level: "3", label: "Byretterne" },
-        { id: 5,  font: { size: 12 },level: "4", label: "Sø og\nhandelsretten" },
-        { id: 6,  font: { size: 12 },level: "4", label: "Grønlands-\ndomstolen" },
-        { id: 7,  font: { size: 12 },level: "4", label: "Retten på\nFærøerne" },
-        { id: 8,  font: { size: 12 },level: "1", label: " Procesbevillingsnævnet \n Den særlige klageret \n Dommerudnævnelsesrådet \n Bibeskæftigelsesnævnet" },
+        { id: 1, font: { size: 20 }, level: "1", label: "Højesteret" },
+        { id: 2, font: { size: 18 }, level: "2", label: "Vestre\nLandsret" },
+        { id: 3, font: { size: 18 }, level: "2", label: "Østre\nLandsret" },
+        { id: 4, font: { size: 18 }, level: "3", label: "Byretterne" },
+        {
+            id: 5,
+            font: { size: 12 },
+            level: "4",
+            label: "Sø og\nhandelsretten",
+        },
+        {
+            id: 6,
+            font: { size: 12 },
+            level: "4",
+            label: "Grønlands-\ndomstolen",
+        },
+        { id: 7, font: { size: 12 }, level: "4", label: "Retten på\nFærøerne" },
+        {
+            id: 8,
+            font: { size: 12 },
+            level: "1",
+            label:
+                " Procesbevillingsnævnet \n Den særlige klageret \n Dommerudnævnelsesrådet \n Bibeskæftigelsesnævnet",
+        },
     ]);
     var nodes13 = new vis.DataSet([
-        { id: 1, font: { size: 25 }, level: "1", label: "Byrettens afdelinger" },
+        {
+            id: 1,
+            font: { size: 25 },
+            level: "1",
+            label: "Byrettens afdelinger",
+        },
         { id: 2, font: { size: 14 }, level: "2", label: "Civilretten" },
         { id: 3, font: { size: 14 }, level: "2", label: "Fogedretten" },
         { id: 4, font: { size: 14 }, level: "2", label: "Skifteretten" },
@@ -57,24 +78,37 @@ function visScript() {
         { id: 6, font: { size: 14 }, level: "2", label: "Kriminalretten" },
     ]);
     var nodes31 = new vis.DataSet(
-        options =
-        [
-        { id: 1, value: 10, level: "1", label: "Aftaleloven" },
-        { id: 2, value: 10, level: "2", label: "Indgåelse af forsikringsaftaler" },
-        { id: 3, value: 10, level: "2", label: "Indgåelse af kreditaftaler" },
-        { id: 4, value: 10, level: "2", label: "Forbrugeraftaler" },
-        { id: 5, value: 10, level: "2", label: "Aftaler om pant" },
-        { id: 6, value: 10, level: "2", label: "Aftaler om køb" },
-        { id: 7, value: 10, level: "2", label: "Aftaler om kautionsformer" },
-    ]);
+        (options = [
+            { id: 1, value: 10, level: "1", label: "Aftaleloven" },
+            {
+                id: 2,
+                value: 10,
+                level: "2",
+                label: "Indgåelse af forsikringsaftaler",
+            },
+            {
+                id: 3,
+                value: 10,
+                level: "2",
+                label: "Indgåelse af kreditaftaler",
+            },
+            { id: 4, value: 10, level: "2", label: "Forbrugeraftaler" },
+            { id: 5, value: 10, level: "2", label: "Aftaler om pant" },
+            { id: 6, value: 10, level: "2", label: "Aftaler om køb" },
+            {
+                id: 7,
+                value: 10,
+                level: "2",
+                label: "Aftaler om kautionsformer",
+            },
+        ])
+    );
 
-    // create an array with edges
+    //create an array with edges
     var edges11 = new vis.DataSet([
         { from: 1, to: 2 },
         { from: 2, to: 3 },
         { from: 3, to: 1 },
-
-
     ]);
     var edges12 = new vis.DataSet([
         { from: 1, to: 2 },
@@ -109,7 +143,6 @@ function visScript() {
     var container13 = document.getElementById("vis13");
     var container31 = document.getElementById("vis31");
 
-
     // provide the data in the vis format
     var data11 = {
         nodes: nodes11,
@@ -124,8 +157,6 @@ function visScript() {
         edges: edges13,
     };
     var data31 = {
-
-
         nodes: nodes31,
         edges: edges31,
     };
@@ -310,7 +341,6 @@ function visScript() {
     var vis12 = new vis.Network(container12, data12, options);
     var vis13 = new vis.Network(container13, data13, options);
     var vis31 = new vis.Network(container31, data31, options2);
-
 }
 
 export default customJS;
