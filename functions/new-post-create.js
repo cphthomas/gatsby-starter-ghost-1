9fda3489e9477b7fdee7bab263500b316deaf445
007AMAN007 <as47986@gmail.com>
2021-06-12 18:08:44 +0530

    test new

--- functions/new-post-create.js
+++ functions/new-post-create.js
@@ -1,21 +1,52 @@
-exports.handler = async function ({ body, headers }, context) {
-    //try {
-    console.log("Yes this works!!!");
-    // console.log("----------------body--------------");
-    // console.log(body);
-    // console.log("-------------headers--------------");
-    // console.log(headers);
-    // if (true) {
-    throw new Error(`You can't post this error`);
-    //}
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ received: subscription.customer }),
-    };
-    // } catch (err) {
-    //     return {
-    //         statusCode: 400,
-    //         body: `Webhook Error: ${err.message}`,
-    //     };
-    // }
-};
+const GhostContentAPI = require("@tryghost/content-api");
+//const sgMail = require('@sendgrid/mail');
+const axios = require("axios");
+
+//sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const ghost = new GhostContentAPI({
+    url: "https://ghost-latest.herokuapp.com",
+    //key: process.env.GHOST_CONTENT_KEY,
+    version: "v3",
+});
+
+async function handler(event, context, callback) {
+    const done = (err, res) =>
+        callback(null, {
+            statusCode: err ? "400" : "200",
+            body: err ? err.message : JSON.stringify(res),
+            headers: { "Content-Type": "application/json" },
+        });
+
+    switch (event.httpMethod) {
+        case "POST":
+            // const payload = JSON.parse(event.body);
+
+            // const emailData = await ghost.pages.read(
+            //     { slug: "nl-welcome-email" },
+            //     { include: "authors" }
+            // );
+            // const emailHtml = (
+            //     await axios.get(`${process.env.GHOST_HOST}/nl-welcome-email/`, {
+            //         responseType: "text",
+            //     })
+            // ).data;
+
+            // const msg = {
+            //   to: payload.subscriber.current.email,
+            //   from: `${emailData.primary_author.name} from Spooky Ghost Stories <geeks@snipcart.com>`,
+            //   subject: emailData.title,
+            //   html: emailHtml,
+            // };
+
+            //await sgMail.send(msg);
+
+            done("Method not supported");
+            break;
+
+        default:
+            done("Method not supported");
+    }
+}
+
+exports.handler = handler;
