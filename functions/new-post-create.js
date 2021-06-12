292d844ada78990227df658397fa4c7966843b45
007AMAN007 <as47986@gmail.com>
2021-06-12 17:32:11 +0530

    test

--- functions/new-post-create.js
+++ functions/new-post-create.js
@@ -1,21 +1,21 @@
 exports.handler = async function ({ body, headers }, context) {
-    try {
-        console.log("Yes this works!!!");
-        console.log("----------------body--------------");
-        console.log(body);
-        console.log("-------------headers--------------");
-        console.log(headers);
-        if (true) {
-            throw new Error(`You can't post this error`);
-        }
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ received: subscription.customer }),
-        };
-    } catch (err) {
-        return {
-            statusCode: 400,
-            body: `Webhook Error: ${err.message}`,
-        };
-    }
+    //try {
+    console.log("Yes this works!!!");
+    // console.log("----------------body--------------");
+    // console.log(body);
+    // console.log("-------------headers--------------");
+    // console.log(headers);
+    // if (true) {
+    throw new Error(`You can't post this error`);
+    //}
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ received: subscription.customer }),
+    };
+    // } catch (err) {
+    //     return {
+    //         statusCode: 400,
+    //         body: `Webhook Error: ${err.message}`,
+    //     };
+    // }
 };
